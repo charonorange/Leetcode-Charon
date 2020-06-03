dbd007ceffef56b74a44c813518d092b5ae36468
赵娜 <wudinana@zhaonadeMacBook-Pro.local>
2020-06-03 10:11:43 +0800

    双指针

--- leetcode/283.js
+++ leetcode/283.js
@@ -10,3 +10,17 @@ var moveZeroes = function(nums) {
     }
     return nums;
 };
+
+// 时间快一点
+var moveZeroes = function(nums) {
+    let j = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[j] = nums[i];
+            if (i !== j) {
+                nums[i] = 0
+            }
+            j++
+        }
+    }
+}
