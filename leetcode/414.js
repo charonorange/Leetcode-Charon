47be5c3a3082740c00f4c2109f8dab02946b2b5e
赵娜 <wudinana@zhaonadeMacBook-Pro.local>
2020-06-08 00:48:33 +0800

    第三大的数

---
+++ leetcode/414.js
@@ -0,0 +1,27 @@
+// 第三大的数
+
+// byMyself
+var thirdMax = function(nums) {
+    var map = new Map()
+    var max = 0
+    nums.sort((a,b) => b - a)
+    if (Array.from(new Set(nums)).length < 3) {
+        var arr = Array.from(new Set(nums))
+        console.log(arr)
+        return arr[0]
+    }
+    for(var i = 0; i< nums.length; i++ ){
+        if (!map.get(nums[i])) {
+            map.set(nums[i], 1)
+            max = nums[i]
+        } else {
+            map.set(nums[i], map.get(nums[i]) + 1)
+            max = nums[i]
+        }
+        if (!(map.size < 3) && map.get(max) === 1) {
+            return max
+        } else {
+            continue
+        }
+    }
+};
