010784b318310d650d9d8e11e8dbb51dd819ba3d
赵娜 <wudinana@zhaonadeMacBook-Pro.local>
2020-06-12 01:54:35 +0800

    用两个数组实现队列

---
+++  Interview/09.js
@@ -0,0 +1,28 @@
+// 用两个数组实现队列
+
+var CQueue = function() {
+    this.stack1 = []
+    this.stack2 = []
+};
+
+/** 
+ * @param {number} value
+ * @return {void}
+ */
+CQueue.prototype.appendTail = function(value) {
+    this.stack1.push(value)
+};
+
+/**
+ * @return {number}
+ */
+CQueue.prototype.deleteHead = function() {
+    if (this.stack2.length) {
+        return this.stack2.pop()
+    }
+    if(!this.stack1.length) return -1
+    while(this.stack1.length) {
+        this.stack2.push(this.stack1.pop())
+    }
+    return this.stack2.pop()
+};
